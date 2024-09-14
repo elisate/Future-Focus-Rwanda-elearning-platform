ec3b1dca335d764a38ed232cfe8ef9cb6193913a
elisate <dushimiyimanaelisa@gmail.com>
2024-09-14 23:50:11 +0200

    fix security

--- src/dashboards/pages/Dashboardlayout.jsx
+++ src/dashboards/pages/Dashboardlayout.jsx
@@ -3,13 +3,19 @@ import { Outlet, Navigate } from "react-router-dom";
 import NavPart from "./NavPart";
 import Sidebar from "./Sidebar";
 
-function Dashboardlayout() {
-  // Assuming a token is stored in localStorage upon successful login
+function DashboardLayout() {
+  // Assuming a token and user role are stored in localStorage upon successful login
   const isAuthenticated = localStorage.getItem("userToken"); // Or any other logic for auth
+  const userRole = localStorage.getItem("role"); // Retrieve user role from storage
 
   if (!isAuthenticated) {
     // If the user is not authenticated, redirect to the login page
     return <Navigate to="/login" />;
+  }
+
+  if (userRole !== "isAdmin") {
+    // If the user does not have the 'isAdmin' role, redirect to the landing page
+    return <Navigate to="/landing" />;
   }
 
   return (
@@ -21,4 +27,4 @@ function Dashboardlayout() {
   );
 }
 
-export default Dashboardlayout;
+export default DashboardLayout;
